9ab35267fba77b6f4b92f9f1bf1178b01f5cc69e
May P <mayppong@gmail.com>
2014-05-12 22:14:09 -0700

    Test ul shadow elem is hidden by default
    Create a module for testing things that should happen on element
    creation. Define setup and teardown for the module.
    Have to switch to inject the element to the page instead of using
    document.createElement() because apparently that doesn't get the style
    applied to. There's no way to test the style of the element. The
    injected element is removed as part of the tear down.

--- testing/tests.js
+++ testing/tests.js
@@ -1,12 +1,28 @@
-
-
-test( 'combobox includes all the shadow elements', function() {
-
-  var combobox = document.createElement('combo-box');
-  var shadowDOM = combobox.shadowRoot;
-
-  ok( shadowDOM.querySelector('input'), 'found and input field.' );
-  ok( shadowDOM.querySelector('button'), 'found the button.' );
-  ok( shadowDOM.querySelector('ul'), 'found the ul holder.')
-
+module('Init', {
+  setup: function() {
+    var cbox = "\
+    <combo-box>\
+      <li>Canada</li>\
+      <li>America</li>\
+      <li>Thailand</li>\
+    </combo-box>\
+    ";
+    jQuery("body").append(cbox);
+  },
+  teardown: function() {
+    jQuery("combo-box").remove();
+  }
 });
+  test( 'combobox includes all the shadow elements', function() {
+    var shadowDOM = document.querySelector('combo-box').shadowRoot;
+
+    ok( shadowDOM.querySelector('input'), 'found and input field.' );
+    ok( shadowDOM.querySelector('button'), 'found the button.' );
+    ok( shadowDOM.querySelector('ul'), 'found the ul holder.')
+  });
+
+  test( 'combobox hides the list by default', function() {
+    var ul = document.querySelector('combo-box').shadowRoot.querySelector('ul');
+
+    equal( jQuery(ul).css('display'), 'none', 'the ul is hidden.');
+  });
