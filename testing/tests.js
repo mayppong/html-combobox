6197d2fa2678d13c48fc323aee939127abd3e6e2
May P <mayppong@gmail.com>
2014-05-13 18:38:49 -0700

    Copy value to input when select an option
    Copy the value from the item a user selected from the list to the shadow
    input field.

--- testing/tests.js
+++ testing/tests.js
@@ -1,4 +1,4 @@
-module('Init', {
+module( 'Init', {
   setup: function() {
     var cbox = "\
     <combo-box>\
@@ -18,11 +18,42 @@ module('Init', {
 
     ok( shadowDOM.querySelector('input'), 'found and input field.' );
     ok( shadowDOM.querySelector('button'), 'found the button.' );
-    ok( shadowDOM.querySelector('ul'), 'found the ul holder.')
+    ok( shadowDOM.querySelector('ul'), 'found the ul holder.' )
   });
 
   test( 'combobox hides the list by default', function() {
     var ul = document.querySelector('combo-box').shadowRoot.querySelector('ul');
 
     equal( jQuery(ul).css('display'), 'none', 'the ul is hidden.');
+  });
+
+module( 'User Interaction', {
+  setup: function() {
+    var cbox = "\
+    <combo-box>\
+      <li>Canada</li>\
+      <li>America</li>\
+      <li>Thailand</li>\
+    </combo-box>\
+    ";
+    jQuery("body").append(cbox);
+  },
+  teardown: function() {
+    jQuery("combo-box").remove();
+  }
+});
+
+  test( 'the options are displayed when combo-box element is clicked on', function() {
+    jQuery('combo-box').click();
+    var ul = document.querySelector('combo-box').shadowRoot.querySelector('ul');
+
+    ok( jQuery(ul).css('display') !== 'none', 'the ul is displayed.');
+  });
+
+  test( 'the value of the selected option is placed in the input field', function() {
+    var cbox = document.querySelector('combo-box');
+    jQuery(cbox).click();
+    var selectedValue = jQuery('li:first', cbox).click().text();
+
+    equal( cbox.shadowRoot.querySelector('input').value, selectedValue, 'the selected value is added to the input field' );
   });
