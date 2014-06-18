44f9c791e5425c1b41474f69cce963bac71e3c2b
May P <mayppong@gmail.com>
2014-06-17 20:06:32 -0700

    Refactor code
    Create new event handlers that calls just the functions that response to
    the events. This makes the code much more testable.
    Create new test units for those functions.
    Switch from focusin to focus again.

--- testing/tests.js
+++ testing/tests.js
@@ -1,4 +1,5 @@
-module( 'Init', {
+
+module( 'Object Creation', {
   setup: function() {
     var cbox = "\
     <combo-box>\
@@ -16,7 +17,7 @@ module( 'Init', {
   test( 'combobox includes all the shadow elements', function() {
     var shadowDOM = document.querySelector('combo-box').shadowRoot;
 
-    ok( shadowDOM.querySelector('input'), 'found and input field.' );
+    ok( shadowDOM.querySelector('input'), 'found an input field.' );
     equal( shadowDOM.querySelector('span').innerHTML, String.fromCharCode(9660), 'found the arrow character.' );
     ok( shadowDOM.querySelector('ul'), 'found the ul holder.' )
   });
@@ -25,6 +26,44 @@ module( 'Init', {
     var ul = document.querySelector('combo-box').shadowRoot.querySelector('ul');
 
     equal( jQuery(ul).css('display'), 'none', 'the ul is hidden.');
+  });
+
+
+module( 'Event Handlers', {
+  setup: function() {
+    var cbox = "\
+    <combo-box>\
+      <li>Canada</li>\
+      <li>America</li>\
+      <li>Thailand</li>\
+    </combo-box>\
+    ";
+    jQuery("body").append(cbox);
+  },
+  teardown: function() {
+    jQuery("combo-box").remove();
+  }
+});
+
+  test( '_showOptions / _hideOptions', function() {
+    var comboBox = document.querySelector('combo-box');
+    var shadowList = comboBox.shadowRoot.querySelector('ul');
+
+    equal( shadowList.style.display, "", 'options are hidden by default.' );
+    comboBox._showOptions();
+    equal( shadowList.style.display, "block", 'options are displayed when calls _showOptions().' );
+    comboBox._hideOptions();
+    equal( shadowList.style.display, "none", 'options are hidden when calls _hideOptions().' );
+  });
+
+  test(' _selectOption', function() {
+    var comboBox = document.querySelector('combo-box');
+    var shadowInput = comboBox.shadowRoot.querySelector('input');
+    var option = comboBox.querySelector('li');
+    var e = { target: option };
+
+    comboBox._selectOption(e);
+    equal(shadowInput.value, option.innerHTML, 'selected option text is placed in the input box.');
   });
 
 
@@ -57,14 +96,22 @@ module( 'User Interaction', {
     equal( jQuery(ul).css('display'), 'none', 'the ul is hidden again when the combobox loses focus.' ); 
   });
 
+/**
+ * Unable to mock the events for testing
+ *
   test( 'selecting options', function() {
     var cbox = document.querySelector('combo-box');
     var ul   = cbox.shadowRoot.querySelector('ul');
-    var focusin = new FocusEvent('focusin');
-    var mousedown = new MouseEvent("mousedown");
+    var focus = new FocusEvent('focus');
+    var mousedown = new MouseEvent("mousedown", {
+      canBubble: true,
+      detail: 1,
+      view: window,
+    });
 
-    cbox.dispatchEvent(focusin);
+    cbox.dispatchEvent(focus);
     cbox.querySelector('li').dispatchEvent(mousedown);
 
     equal( cbox.shadowRoot.querySelector('input').value, 'Canada', 'the selected value is added to the input field' );
   });
+*/
